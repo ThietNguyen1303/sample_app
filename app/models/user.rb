4fe293af8c4cc62af30864245aac42ca9ee52885
YOUR NAME <YOUR@EMAIL.com>
2019-04-17 09:37:40 +0700

    chapter6

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name,  presence: true, length: {maximum: Settings.user_valid.max_length_name}
+  validates :email, presence: true, length: {maximum: Settings.user_valid.max_length_email},
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length:{minimum: Settings.user_valid.min_length_password}
+  has_secure_password
+
+  private
+  def email_downcase
+    email.downcase!
+  end
+end
