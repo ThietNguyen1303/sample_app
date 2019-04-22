fc87c4ef19599f21562355423dbb2a5aa68f26d2
YOUR NAME <YOUR@EMAIL.com>
2019-04-22 10:42:38 +0700

    chapter9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,10 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+    if user&.authenticate(params[:session][:password])
+      flash.now[:success] = t ".success"
+
+      login_with_remember user
     else
       flash[:danger] = t "controller.sessions.wrongmail_password"
       render :new
@@ -14,7 +15,13 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+   def login_with_remember user
+    log_in user
+    remember_me = params[:session][:remember_me]
+    remember_me == Settings.checkbox ? remember(user) : forget(user)
+    redirect_to user
   end
 end
